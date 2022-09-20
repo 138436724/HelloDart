c974b755de64dc88972ef03a660b820f61ea4c54
FireFight <2268561366@qq.com>
2022-09-20 09:24:15 +0000

    new file

---
+++ 01_dart_lang_variables.dart
@@ -0,0 +1,22 @@
+// {https://dart.cn/guides/language/language-tour#variables}
+
+void var_name() {
+  var name = 'Bob';
+  print('var name is $name');
+}
+
+void Object_name() {
+  Object name = 'Bob';
+  print('Object name is $name');
+}
+
+void String_name() {
+  String name = 'Bob';
+  print('String name is $name');
+}
+
+void main() {
+  var_name();
+  Object_name();
+  String_name();
+}
