d23b0f10d20b303dded462809a94b9b5645e0e8f
FireFight <2268561366@qq.com>
2022-10-04 08:40:02 +0000

    add

--- 01_language-tour/02_built-in-types/01_number.dart
+++ 01_language-tour/02_built-in-types/01_number.dart
@@ -1,7 +1,7 @@
 // https://dart.cn/guides/language/language-tour#built-in-types
 // https://dart.cn/guides/language/language-tour#numbers
 
-void main(List<String> args) {
+void number() {
   var x = 1;
   var hex = 0xDEADBEEF;
 
@@ -19,30 +19,37 @@ void main(List<String> args) {
 
 void change_type() {
   // String -> int
-var one = int.parse('1');
-assert(one == 1);
+  var one = int.parse('1');
+  assert(one == 1);
 
 // String -> double
-var onePointOne = double.parse('1.1');
-assert(onePointOne == 1.1);
+  var onePointOne = double.parse('1.1');
+  assert(onePointOne == 1.1);
 
 // int -> String
-String oneAsString = 1.toString();
-assert(oneAsString == '1');
+  String oneAsString = 1.toString();
+  assert(oneAsString == '1');
 
 // double -> String
-String piAsString = 3.14159.toStringAsFixed(2);
-assert(piAsString == '3.14');
+  String piAsString = 3.14159.toStringAsFixed(2);
+  assert(piAsString == '3.14');
 }
 
-
-assert((3 << 1) == 6); // 0011 << 1 == 0110
-assert((3 | 4) == 7); // 0011 | 0100 == 0111
-assert((3 & 4) == 0); // 0011 & 0100 == 0000
-// 更多示例请查看 移位操作符 小节。
+void bitwise_shift() {
+  assert((3 << 1) == 6); // 0011 << 1 == 0110
+  assert((3 | 4) == 7); // 0011 | 0100 == 0111
+  assert((3 & 4) == 0); // 0011 & 0100 == 0000
+}
 
 // 数字字面量为编译时常量。很多算术表达式只要其操作数是常量，则表达式结果也是编译时常量。
 
 const msPerSecond = 1000;
 const secondsUntilRetry = 5;
 const msUntilRetry = secondsUntilRetry * msPerSecond;
+
+void main(List<String> args) {
+  number();
+  change_type();
+  bitwise_shift();
+  print(msUntilRetry);
+}
