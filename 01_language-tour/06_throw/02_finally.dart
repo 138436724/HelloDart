872757d5129ca901ef5642f81cf33c9c540a382f
FireFight <2268561366@qq.com>
2022-12-13 13:13:52 +0000

    add

---
+++ 01_language-tour/06_throw/02_finally.dart
@@ -0,0 +1,19 @@
+void misbehave() {
+  try {
+    dynamic foo = true;
+    print(foo++); // Runtime error
+  } catch (e) {
+    print('misbehave() partially handled ${e.runtimeType}.');
+    rethrow; // Allow callers to see the exception.
+  } finally {
+    print('cleanLlamaStalls'); // Then clean up.
+  }
+}
+
+void main() {
+  try {
+    misbehave();
+  } catch (e) {
+    print('main() finished handling ${e.runtimeType}.');
+  }
+}
